3cbc8fec834bae567708532ade6d820d468980f4
acarboni <andrea.carboni71@gmail.com>
2026-01-20 17:48:27 +0100

    Renamed tradalia to algotiqa

--- schema/create-inventory.sh
+++ schema/create-inventory.sh
@@ -3,8 +3,9 @@
 dbName=inventory
 dbUser=admin
 dbPass=admin
+dbPort=3400
 
-mysql --user=root --password=root --host=127.0.0.1 --port=3400 << EOF
+mysql --user=root --password=root --host=127.0.0.1 --port=$dbPort << EOF
 drop database if exists ${dbName};
 create database ${dbName} character set = 'utf8' collate = 'utf8_bin';
 
@@ -12,4 +13,4 @@ grant references,alter,create,drop,index,select,update,delete,insert on ${dbName
 grant references,alter,create,drop,index,select,update,delete,insert on ${dbName}.* to '${dbUser}'@'%'         identified by '${dbPass}';
 EOF
 
-mysql --user=$dbUser --password=$dbPass --host=127.0.0.1 --port=3400 $dbName < $dbName-schema.sql
+mysql --user=$dbUser --password=$dbPass --host=127.0.0.1 --port=$dbPort $dbName < $dbName-schema.sql
